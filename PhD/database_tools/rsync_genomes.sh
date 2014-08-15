1ff8a46980ccdea8f78f8fb00fe2036aabc73ea6
Finlay Maguire <finlaymaguire@gmail.com>
2014-08-15 16:15:55 +0100

    synchronise with blastdb on migard

---
+++ PhD/database_tools/rsync_genomes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rsync -n -zPav --delete midgard:~/Desktop/genomes/cider/ /genomes/cider
+
+read -p "Does the above look right? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rsync -zPav --delete midgard:~/Desktop/genomes/cider/ /genomes/cider
+    ls -1 /genomes/cider/*.fas.psq > species_list.txt
+    sed -i 's/.fas.psq//g' species_list.txt
+    sed -i 's/\/genomes\/cider\///g' species_list.txt
+    sort species_list.txt > species_list_sorted.txt
+    mv species_list_sorted.txt species_list.txt
+fi
+
