90ffb3c37ca04d98641f57a96be709e9a6726ba2
Finlay Maguire <finlaymaguire@gmail.com>
2014-09-15 14:52:08 +0100

    upgraded sync tool to use more concurrency in sorting and chunking the taxonomy lookup

--- PhD/database_tools/rsync_genomes.sh
+++ PhD/database_tools/rsync_genomes.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-rsync -n -zPav --delete midgard:~/Desktop/genomes/cider/ /genomes/cider
+rsync -n -zPav --size-only --delete midgard:~/Desktop/genomes/cider/ /genomes/cider
 
 read -p "Does the above look right? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    rsync -zPav --delete midgard:~/Desktop/genomes/cider/ /genomes/cider
-    ls -1 /genomes/cider/*.fas.psq > species_list.txt
-    sed -i 's/.fas.psq//g' species_list.txt
-    sed -i 's/\/genomes\/cider\///g' species_list.txt
-    sort species_list.txt > species_list_sorted.txt
+    echo "Retrieving files"
+    rsync -zPav --size-only --delete midgard:~/Desktop/genomes/cider/ /genomes/cider
+    echo "Generating species list"
+    ls -1 /genomes/cider/*.fas.psq | grep -oP '(?<=\/genomes\/cider\/).*?(?=\.fas\.psq|$)' \
+        > species_list.txt
+    echo "Sorting species list"
+    sort --parallel=10 species_list.txt > species_list_sorted.txt
     mv species_list_sorted.txt species_list.txt
-    ./get_taxonomy_for_species_list.py species_list.txt taxonomy_list.txt
+    echo "Generating taxonomy list"
+    split -n l/10 species_list.txt split_
+    for i in split_*; do
+        ./get_taxonomy_for_species_list.py ${i} ${i}_taxonomy_list.txt &
+    done
+    wait
+    cat split_*_taxonomy_list.txt > taxonomy_list.txt
+    rm split_*
+    sort --parallel=10 taxonomy_list.txt > taxonomy_list_sorted.txt
+    mv taxonomy_list_sorted.txt taxonomy_list.txt
 fi
 
